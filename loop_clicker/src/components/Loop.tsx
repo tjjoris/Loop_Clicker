0cfa1ea8e2b3af0f7b29bcb51eaa2dd8d1c1667e
tjjoris <tjjoris@gmail.com>
2025-03-01 14:57:46 -0500

    imported placeholder sprites, and added Loop.tsx

---
+++ loop_clicker/src/components/Loop.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import elastic from "../assets/elastic_placeholder.png";
+
+export default function Loop () {
+    return (
+        <>
+            <img 
+            src={elastic}
+            />
+        </>
+    )
+}
