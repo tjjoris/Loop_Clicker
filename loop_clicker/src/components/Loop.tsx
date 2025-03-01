1b50a4ae545b76a5ada18de58897096ce028300d
tjjoris <tjjoris@gmail.com>
2025-03-01 16:41:23 -0500

    added variable values for width, height, x, and y for loop and loop bunch components.

--- loop_clicker/src/components/Loop.tsx
+++ loop_clicker/src/components/Loop.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 
 export default function Loop () {
+    let left : number = 100;
+    let top : number = 100;
+    let width: number = 100;
+    let height: number = 100;
     return (
         <>
             <img 
             src={`/src/assets/elastic_placeholder.png`}
+            style={{
+                left: `${left}px`,
+                top: `${top}px`,
+                position: "absolute",
+                width: `${width}px`,
+                height: `${height}px`
+            }}
             />
         </>
     )
