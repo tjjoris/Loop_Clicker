ee2256c99a515f77d2cabe420aac42350f70dfde
tjjoris <tjjoris@gmail.com>
2025-03-01 15:03:12 -0500

    imported LoopBunch and created LoopBunch.tsx

--- loop_clicker/src/App.tsx
+++ loop_clicker/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Loop from './components/Loop'
+import LoopBunch from './components/LoopBunch'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Loop/>
+      <LoopBunch/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ loop_clicker/src/components/LoopBunch.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import elastic_ball from "../assets/elastic_ball_placeholder.png";
+
+export default function LoopBunch() {
+    return (
+        <>
+            <img
+            src={elastic_ball}
+            />
+        </>
+    )
+}
