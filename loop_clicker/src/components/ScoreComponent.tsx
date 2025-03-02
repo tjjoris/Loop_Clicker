bdde6fd2d67410ce8bfcafd962eb0d3e4a20a691
tjjoris <tjjoris@gmail.com>
2025-03-02 15:02:41 -0500

    added logic for displaying numbers in the millions.

--- loop_clicker/src/components/ScoreComponent.tsx
+++ loop_clicker/src/components/ScoreComponent.tsx
@@ -4,11 +4,18 @@ import Score from "../oop/game/Score";
 export default function ScoreComponent({scoreObject}: {scoreObject: Score}) {
     //set the number to the object state value
     const score: number = useScoreStore(scoreObject);
+    let scoreDisplay = Math.floor(score);
+    let decimalIndicator: string = ""
+    if (scoreDisplay >= 1000000) {
+        scoreDisplay = (scoreDisplay / 1000);
+        decimalIndicator = " million";
+    }
+    let scoreDisplaySting: string = scoreDisplay.toString() + decimalIndicator;
     
     return (
         <>
             <h1>
-                Score: {score}
+                Score: {scoreDisplaySting}
             </h1>
         </>
     )
--- loop_clicker/src/oop/upgrades/ScissorsUpgrade.ts
+++ loop_clicker/src/oop/upgrades/ScissorsUpgrade.ts
@@ -5,6 +5,6 @@ export default class ScissorsUpgrade extends Upgrade {
 
     constructor(score: Score) {
         super(score);
-        this.state = {name: "scissors", cost: 3, iterationAmount: 1, count: 0}    
+        this.state = {name: "scissors", cost: 3, iterationAmount: 0.01, count: 0}    
     }
 }
