2a45f1d271b590b6d4662e0013baa3fa5a384f8f
tjjoris <tjjoris@gmail.com>
2025-03-07 14:42:46 -0500

    moving UpgrdesComponent.tsx CSS into App.css

--- loop_clicker/src/components/UpgradesComponent.tsx
+++ loop_clicker/src/components/UpgradesComponent.tsx
@@ -9,13 +9,14 @@ export default function UpgradesComponent({upgrades}: {upgrades: Upgrades}) {
 
     return (
         <div
-            style={{
-                // position:"absolute",
-                // left: `${x}px`,
-                // top:`${y}px`,
-                display: "flex",
-                flexDirection: "column"
-            }}
+            className="upgradesComponent"
+            // style={{
+            //     // position:"absolute",
+            //     // left: `${x}px`,
+            //     // top:`${y}px`,
+            //     display: "flex",
+            //     flexDirection: "column"
+            // }}
         >
             {state.map((upgrade, index) => {
                 return <UpgradeComponent key = {index} upgrade={upgrade} index={index}/>;
